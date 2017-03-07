70a683d0e34785b38ef0ded18024a61444863011
Daniel <dkuhrmeyer@gmail.com>
2017-03-07 08:06:56 -0700

    made minor changes and got image Capture to work

--- app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/MainActivity.java
+++ app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/MainActivity.java
@@ -1,10 +1,12 @@
 package tooearly.neumont.edu.imagefilter.Activities;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageButton captureImage;
     ImageView cameraView;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -25,14 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
         captureImage = (ImageButton)findViewById(R.id.captureImage);
         cameraView = (ImageView)findViewById(R.id.cameraView);
-    }
 
-    public void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        if(!hasCamera()){
+            captureImage.setEnabled(false);
         }
     }
+
+    public boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+
+    public void launchCamera(View v){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {

d6ff9be453b239d505ed0f31bbe950fc5c4baf5a
Daniel <dkuhrmeyer@gmail.com>
2017-03-07 07:43:10 -0700

    designed layout for 3 of 4 pages and started logic for capturing an image

--- app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/MainActivity.java
+++ app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/MainActivity.java
@@ -1,14 +1,45 @@
 package tooearly.neumont.edu.imagefilter.Activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import tooearly.neumont.edu.imagefilter.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton captureImage;
+    ImageView cameraView;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureImage = (ImageButton)findViewById(R.id.captureImage);
+        cameraView = (ImageView)findViewById(R.id.cameraView);
+    }
+
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraView.setImageBitmap(imageBitmap);
+        }
     }
 }

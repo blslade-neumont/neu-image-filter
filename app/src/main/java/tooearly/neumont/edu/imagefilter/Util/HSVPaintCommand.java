92475649089dca6858362e5c8594b9a9d88a1b2f
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-08 07:34:25 -0700

    feat(hsv-paint-command): multiply the ColorMatrix to shift the hue

--- app/src/main/java/tooearly/neumont/edu/imagefilter/Util/HSVPaintCommand.java
+++ app/src/main/java/tooearly/neumont/edu/imagefilter/Util/HSVPaintCommand.java
@@ -8,17 +8,33 @@ public class HSVPaintCommand extends ColorMatrixCommand {
     }
 
     private static ColorMatrix createColorMatrix(float hue, float saturation, float value) {
-        if (hue != 0) throw new IllegalStateException("Can't shift hue! Not implemented!");
         ColorMatrix matrix = new ColorMatrix(new float[] {
                 1, 0, 0, 0, value,
                 0, 1, 0, 0, value,
                 0, 0, 1, 0, value,
-                0, 0, 0, 1, 0
+                0, 0, 0, 1, 0,
+                0, 0, 0, 0, 1
         });
         if (saturation != 0) {
             ColorMatrix saturationMatrix = new ColorMatrix();
             saturationMatrix.setSaturation(saturation);
-            matrix.preConcat(saturationMatrix);
+            matrix.postConcat(saturationMatrix);
+        }
+        if (hue != 0) {
+            //Mathematical formula to create a "shift-hue" color matrix adapted from http://stackoverflow.com/a/17490633/768597
+            float cosVal = (float)Math.cos(value);
+            float sinVal = (float)Math.sin(value);
+            float lumR = 0.213f;
+            float lumG = 0.715f;
+            float lumB = 0.072f;
+            ColorMatrix hueMatrix = new ColorMatrix(new float[] {
+                    lumR + cosVal * (1 - lumR) + sinVal * (-lumR),    lumG + cosVal * (-lumG) + sinVal * (-lumG),     lumB + cosVal * (-lumB) + sinVal * (1 - lumB), 0, 0,
+                    lumR + cosVal * (-lumR) + sinVal * (0.143f),      lumG + cosVal * (1 - lumG) + sinVal * (0.140f), lumB + cosVal * (-lumB) + sinVal * (-0.283f),  0, 0,
+                    lumR + cosVal * (-lumR) + sinVal * (-(1 - lumR)), lumG + cosVal * (-lumG) + sinVal * (lumG),      lumB + cosVal * (1 - lumB) + sinVal * (lumB),  0, 0,
+                    0f, 0f, 0f, 1f, 0f,
+                    0f, 0f, 0f, 0f, 1f
+            });
+            matrix.postConcat(hueMatrix);
         }
         return matrix;
     }

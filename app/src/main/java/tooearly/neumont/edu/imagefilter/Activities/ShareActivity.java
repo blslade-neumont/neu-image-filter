d42730b4981518cf33733d4c9926948f6299ee92
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-13 22:15:59 -0600

    feat(share-activity): show preview image, attach title and description to share intent

--- app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/ShareActivity.java
+++ app/src/main/java/tooearly/neumont/edu/imagefilter/Activities/ShareActivity.java
@@ -3,15 +3,18 @@ package tooearly.neumont.edu.imagefilter.Activities;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.OutputStream;
 
 import tooearly.neumont.edu.imagefilter.R;
@@ -19,44 +22,67 @@ import tooearly.neumont.edu.imagefilter.Services.BitmapStorageService;
 
 import static tooearly.neumont.edu.imagefilter.Activities.CaptureImageActivity.IMAGE_EXTRA;
 
-/**
- * Created by Daniel Kuhrmeyer on 3/10/2017.
- */
-
 public class ShareActivity extends AppCompatActivity {
+
+    public static String TAG = "ShareActivity";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
 
+        Intent intent = getIntent();
+        bmp = BitmapStorageService.storage.get(intent.getStringExtra(IMAGE_EXTRA));
 
+        ImageView preview = (ImageView)findViewById(R.id.img_preview);
+        preview.setImageBitmap(bmp);
+
+        txt_name = (EditText)findViewById(R.id.txt_name);
+        txt_description = (EditText)findViewById(R.id.photoDescription);
     }
 
-    public void shareScreen(View v){
-        Intent randomVariable = getIntent();
-        String anotherVariable = randomVariable.getStringExtra(IMAGE_EXTRA);
+    private Bitmap bmp;
+    private EditText txt_name, txt_description;
 
-        Bitmap icon = BitmapStorageService.storage.get(anotherVariable);
-        Intent share = new Intent(Intent.ACTION_SEND);
-        share.setType("image/jpeg");
+    public void onShareScreenClicked(View v) {
+        shareScreen();
+    }
+    public void shareScreen() {
+        String title = txt_name.getText().toString();
+        String description = txt_description.getText().toString();
 
         ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "title");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "description");
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                values);
+        Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        if (uri == null) throw new IllegalStateException();
 
-        OutputStream outstream;
+        OutputStream outstream = null;
         try {
             outstream = getContentResolver().openOutputStream(uri);
-            icon.compress(Bitmap.CompressFormat.JPEG, 100, outstream);
-            outstream.close();
-        } catch (Exception e) {
-            System.err.println(e.toString());
+            if (outstream == null) throw new IllegalStateException();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, outstream);
+        }
+        catch (FileNotFoundException e) {
+            Log.wtf(TAG, e);
+            Toast.makeText(this, R.string.msg_failed_to_share_image, Toast.LENGTH_LONG).show();
+            return;
+        }
+        finally {
+            if (outstream != null) {
+                try {outstream.close();}
+                catch (IOException e) { Log.wtf(TAG, e); }
+            }
         }
 
+        Intent share = new Intent(Intent.ACTION_SEND);
+        //noinspection deprecation
+        share.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        share.setType("image/jpeg");
         share.putExtra(Intent.EXTRA_STREAM, uri);
+        share.putExtra(Intent.EXTRA_TITLE, title);
+        share.putExtra(Intent.EXTRA_SUBJECT, title);
+        share.putExtra(Intent.EXTRA_TEXT, description);
         startActivity(Intent.createChooser(share, "Share Image"));
     }
 }
